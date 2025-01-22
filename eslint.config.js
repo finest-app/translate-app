0dcd0f10f7075e278f4292f65fc7e8a8725aded9
Visualizeit <61679509+Visualizeit@users.noreply.github.com>
2025-01-22 09:30:04 +0800

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+import { config as defaultConfig } from '@epic-web/config/eslint'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+
+/** @type {import("eslint").Linter.Config} */
+export default [...defaultConfig, jsxA11y.flatConfigs.recommended]
