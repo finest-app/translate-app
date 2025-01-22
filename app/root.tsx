928380a349b4714251123c4eedaed9210d81b132
Visualizeit <vnyeit@gmail.com>
2025-01-22 18:24:56 +0800

    Add language configuration and integrate Nuqs adapter for form handling

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import { Theme } from '@radix-ui/themes'
+import { NuqsAdapter } from 'nuqs/adapters/react-router/v7'
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router'
 import './app.css'
 
@@ -14,9 +15,11 @@ export const Layout = () => {
 				<Links />
 			</head>
 			<body>
-				<Theme className="h-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
-					<Outlet />
-				</Theme>
+				<NuqsAdapter>
+					<Theme className="h-full bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+						<Outlet />
+					</Theme>
+				</NuqsAdapter>
 				<ScrollRestoration />
 				<Scripts />
 			</body>
@@ -24,8 +27,4 @@ export const Layout = () => {
 	)
 }
 
-const App = () => {
-	return <Outlet />
-}
-
-export default App
+export default Outlet
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,48 @@
-import { Container, TextArea } from '@radix-ui/themes'
+import { Container, Flex, Card, TextArea } from '@radix-ui/themes'
+import { parseAsString, parseAsStringEnum, useQueryStates } from 'nuqs'
+import { useFetcher } from 'react-router'
+import { type Route } from './+types/_index'
+import languages from '@/configs/languages'
+
+const languageCodes = languages.map((language) => language.code)
+
+export const action = async ({ request }: Route.ActionArgs) => {
+	const formData = await request.formData()
+
+	return formData.get('text')
+}
 
 const HomePage = () => {
+	const fetcher = useFetcher()
+
+	const [, setSearchParams] = useQueryStates({
+		source_lang: parseAsStringEnum(languageCodes).withDefault('en'),
+		target_lang: parseAsStringEnum(languageCodes).withDefault('zh'),
+		text: parseAsString.withDefault(''),
+	})
+
 	return (
-		<Container size="2" p="6">
-			<TextArea size="3" radius="full" placeholder="Type something..." />
+		<Container size="2" p="4">
+			<Flex direction="column" gap="4">
+				<fetcher.Form method="post">
+					<TextArea
+						name="text"
+						size="3"
+						radius="full"
+						placeholder="Type something..."
+						defaultValue={''}
+						onChange={async (event) => {
+							await setSearchParams({ text: event.target.value })
+
+							await fetcher.submit(event.target.form)
+						}}
+					/>
+				</fetcher.Form>
+				{fetcher.state}
+				<Card>
+					<pre>{fetcher.data}</pre>
+				</Card>
+			</Flex>
 		</Container>
 	)
 }
